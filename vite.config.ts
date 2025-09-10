f0babc4c8a592a3707eb1b5de35829b44169c85f
BlckSwn13 <wetwet1409@outlook.com>
2025-09-09 23:35:01 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     alias: {
       '@components': path.resolve(__dirname, 'src/components'),
       '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@types': path.resolve(__dirname, 'src/types'),
-    },
-  },
+      '@types': path.resolve(__dirname, 'src/types')
+    }
+  }
 });
